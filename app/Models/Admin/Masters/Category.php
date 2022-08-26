1ab4847eeeec06a7dfd6a95f9ca3582eff9ff45b
FiqriNawadata <fiqri.alfayed@nawadata.com>
2022-08-27 00:32:09 +0700

    Change Log 1. Addition PositionController and TagController

---
+++ app/Models/Admin/Masters/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Admin\Masters;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mst_category';
+}
---
+++ app/Models/Admin/Masters/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Admin\Masters;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mst_tag';
+}
